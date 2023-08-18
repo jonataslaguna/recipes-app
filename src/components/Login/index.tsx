a15ee976ef5fbc61550d40e8e26bfedad906c9fa
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-08-18 17:07:45 -0300

    req-3-and-4-implemented checks at login and start of tests

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,18 +1,52 @@
+import { useState } from 'react';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+};
+
 function Login() {
+  const [formLogin, setFormLogin] = useState(INITIAL_STATE);
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, id } = e.target;
+
+    setFormLogin({
+      ...formLogin,
+      [id]: value,
+    });
+  };
+
   return (
-    <form>
+    <form onChange={ handleSubmit }>
+
+      <h1>Login</h1>
+
       <input
         data-testid="email-input"
         type="email"
         id="email"
+        placeholder="E-mail"
+        value={ formLogin.email }
+        onChange={ handleChange }
       />
       <input
         data-testid="password-input"
         type="password"
         id="password"
+        placeholder="Password"
+        value={ formLogin.password }
+        onChange={ handleChange }
       />
       <button
         data-testid="login-submit-btn"
+        disabled={ !emailRegex.test(formLogin.email) || formLogin.password.length < 7 }
       >
         Enter
       </button>
