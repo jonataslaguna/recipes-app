1473c8a1c05084e223c9f2c1fbe4c0705b7567c7
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-08-21 20:17:06 -0300

    req-11-Implemented three radio buttons in the search bar: Ingredient, Name and First letter

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,10 +3,43 @@ import { useState } from 'react';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 import { HeaderRouterProps } from './type';
+import { getFirstLetter, getIngredient, getName } from '../../utils/api';
+
+const InitialState = {
+  searchInput: '',
+  searchType: 'ingredient',
+};
 
 function Header({ pageTitle, showSearchIcon }: HeaderRouterProps) {
+  const [inputsData, setInputsData] = useState(InitialState);
   const [showSearchBtn, setShowSearchBtn] = useState(false);
+  const [recipes, setRecipes] = useState([{}]);
+
   const navigate = useNavigate();
+
+  const handleSubmitFormSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { searchInput, searchType } = inputsData;
+
+    if (searchType === 'ingredient') {
+      setRecipes(await getIngredient(searchInput));
+    } else if (searchType === 'name') {
+      setRecipes(await getName(searchInput));
+    } else if (searchType === 'firstLetter' && searchInput.length === 1) {
+      setRecipes(await getFirstLetter(searchInput));
+    } else {
+      alert('Your search must have only 1 (one) character');
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value, type } = e.target;
+    if (type === 'radio') {
+      setInputsData({ ...inputsData, searchType: id, [id]: value });
+    } else {
+      setInputsData({ ...inputsData, [id]: value });
+    }
+  };
 
   const handleClickSearchButton = () => {
     setShowSearchBtn(!showSearchBtn);
@@ -33,12 +66,16 @@ function Header({ pageTitle, showSearchIcon }: HeaderRouterProps) {
           />
         </button>
       )}
+
       {showSearchBtn && (
-        <div>
+        <form onSubmit={ handleSubmitFormSearch }>
           <input
             type="text"
             data-testid="search-input"
             placeholder="Search recipe"
+            id="searchInput"
+            value={ inputsData.searchInput }
+            onChange={ handleChange }
           />
 
           <input
@@ -46,6 +83,8 @@ function Header({ pageTitle, showSearchIcon }: HeaderRouterProps) {
             type="radio"
             name="search-radio"
             id="ingredient"
+            checked
+            onChange={ handleChange }
           />
           <label htmlFor="ingredient">Ingredient</label>
 
@@ -54,20 +93,23 @@ function Header({ pageTitle, showSearchIcon }: HeaderRouterProps) {
             type="radio"
             name="search-radio"
             id="name"
+            onChange={ handleChange }
           />
           <label htmlFor="name">Name</label>
+
           <input
             data-testid="first-letter-search-radio"
             type="radio"
             name="search-radio"
             id="firstLetter"
+            onChange={ handleChange }
           />
           <label htmlFor="firstLetter">First letter</label>
 
           <button data-testid="exec-search-btn">Search</button>
-        </div>
-      )}
+        </form>
 
+      )}
     </header>
   );
 }
