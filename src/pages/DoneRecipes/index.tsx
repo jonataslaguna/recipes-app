3f748a21d4bbe0875dfa7e3f4f80a2adda9e9e18
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-08-22 11:14:58 -0300

    organizing hearder and routes

---
+++ src/pages/DoneRecipes/index.tsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header';
+
+function DoneRecipes() {
+  return (
+    <div>
+      <Header pageTitle="Done Recipes" />
+    </div>
+  );
+}
+
+export default DoneRecipes;
