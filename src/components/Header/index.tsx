0645f96c078356e91e5f9fd443a4d14da17280cb
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-08-22 08:51:23 -0300

    req-11 organizing the code and correcting the tests

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,58 +3,20 @@ import { useState } from 'react';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 import { HeaderRouterProps } from './type';
-import { getFirstLetter, getIngredient, getName } from '../../utils/api';
-
-const InitialState = {
-  searchInput: '',
-  searchType: 'ingredient',
-};
+import SearchForm from '../SearchForm';
 
 function Header({ pageTitle, showSearchIcon }: HeaderRouterProps) {
-  const [inputsData, setInputsData] = useState(InitialState);
-  const [showSearchBtn, setShowSearchBtn] = useState(false);
-  const [recipes, setRecipes] = useState([{}]);
+  const [showInputSearch, setShowInputSearch] = useState(false);
 
   const navigate = useNavigate();
 
-  const handleSubmitFormSearch = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const { searchInput, searchType } = inputsData;
-
-    if (searchType === 'ingredient') {
-      setRecipes(await getIngredient(searchInput));
-    } else if (searchType === 'name') {
-      setRecipes(await getName(searchInput));
-    } else if (searchType === 'firstLetter' && searchInput.length === 1) {
-      setRecipes(await getFirstLetter(searchInput));
-    } else {
-      alert('Your search must have only 1 (one) character');
-    }
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, value, type } = e.target;
-    if (type === 'radio') {
-      setInputsData({ ...inputsData, searchType: id, [id]: value });
-    } else {
-      setInputsData({ ...inputsData, [id]: value });
-    }
-  };
-
   const handleClickSearchButton = () => {
-    setShowSearchBtn(!showSearchBtn);
+    setShowInputSearch(!showInputSearch);
   };
 
   return (
     <header>
       <h1 data-testid="page-title">{pageTitle}</h1>
-      <button onClick={ () => navigate('/profile') }>
-        <img
-          src={ profileIcon }
-          alt="profile"
-          data-testid="profile-top-btn"
-        />
-      </button>
       {showSearchIcon && (
         <button
           onClick={ handleClickSearchButton }
@@ -66,50 +28,15 @@ function Header({ pageTitle, showSearchIcon }: HeaderRouterProps) {
           />
         </button>
       )}
+      <button onClick={ () => navigate('/profile') }>
+        <img
+          src={ profileIcon }
+          alt="profile"
+          data-testid="profile-top-btn"
+        />
+      </button>
 
-      {showSearchBtn && (
-        <form onSubmit={ handleSubmitFormSearch }>
-          <input
-            type="text"
-            data-testid="search-input"
-            placeholder="Search recipe"
-            id="searchInput"
-            value={ inputsData.searchInput }
-            onChange={ handleChange }
-          />
-
-          <input
-            data-testid="ingredient-search-radio"
-            type="radio"
-            name="search-radio"
-            id="ingredient"
-            checked
-            onChange={ handleChange }
-          />
-          <label htmlFor="ingredient">Ingredient</label>
-
-          <input
-            data-testid="name-search-radio"
-            type="radio"
-            name="search-radio"
-            id="name"
-            onChange={ handleChange }
-          />
-          <label htmlFor="name">Name</label>
-
-          <input
-            data-testid="first-letter-search-radio"
-            type="radio"
-            name="search-radio"
-            id="firstLetter"
-            onChange={ handleChange }
-          />
-          <label htmlFor="firstLetter">First letter</label>
-
-          <button data-testid="exec-search-btn">Search</button>
-        </form>
-
-      )}
+      {showInputSearch && <SearchForm />}
     </header>
   );
 }
---
+++ src/components/SearchForm/index.tsx
@@ -0,0 +1,63 @@
+import { useContext } from 'react';
+import ContextRecipes from '../../context/ContextRecipes';
+
+function SearchForm() {
+  const {
+    searchFormData,
+    handleSubmitSearchForm,
+    setSearchFormData,
+  } = useContext(ContextRecipes);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value, type } = e.target;
+    if (type === 'radio') {
+      setSearchFormData({ ...searchFormData, searchType: id, [id]: value });
+    } else {
+      setSearchFormData({ ...searchFormData, [id]: value });
+    }
+  };
+  return (
+    <form onSubmit={ handleSubmitSearchForm }>
+      <input
+        type="text"
+        data-testid="search-input"
+        placeholder="Search recipe"
+        id="searchInput"
+        value={ searchFormData?.searchInput }
+        onChange={ handleChange }
+      />
+
+      <input
+        data-testid="ingredient-search-radio"
+        type="radio"
+        name="search-radio"
+        id="ingredient"
+        checked
+        onChange={ handleChange }
+      />
+      <label htmlFor="ingredient">Ingredient</label>
+
+      <input
+        data-testid="name-search-radio"
+        type="radio"
+        name="search-radio"
+        id="name"
+        onChange={ handleChange }
+      />
+      <label htmlFor="name">Name</label>
+
+      <input
+        data-testid="first-letter-search-radio"
+        type="radio"
+        name="search-radio"
+        id="firstLetter"
+        onChange={ handleChange }
+      />
+      <label htmlFor="firstLetter">First letter</label>
+
+      <button data-testid="exec-search-btn">Search</button>
+    </form>
+  );
+}
+
+export default SearchForm;
