df19dd87d32fe06320643050bb7de8eb0aa48521
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-08-23 17:55:58 -0300

    Req 57 - inputs implemented

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -3,10 +3,29 @@ import Header from '../../components/Header';
 
 function Profile() {
   return (
-    <div>
+    <>
       <Header pageTitle="Profile" />
+
+      <form>
+        <input type="email" data-testid="profile-email" />
+        <button
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </form>
       <Footer />
-    </div>
+    </>
   );
 }
 
