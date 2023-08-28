342d9af16281789e5e2dc77da95c82c670d68ad2
Rodrigo Bianek <rodrigobianek@gmail.com>
2023-08-28 18:10:34 -0300

    Req23 - finishing tests

---
+++ src/tests/mocks/recipesData.ts
@@ -0,0 +1,249 @@
+export const mealsDataMock = { meals: [
+  {
+    idMeal: '52977',
+    strMeal: 'Corba',
+    strDrinkAlternate: null,
+    strCategory: 'Side',
+    strArea: 'Turkish',
+    strMealThumb: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+  },
+  {
+    idMeal: '53060',
+    strMeal: 'Burek',
+    strDrinkAlternate: null,
+    strCategory: 'Side',
+    strArea: 'Croatian',
+    strMealThumb: 'https://www.themealdb.com/images/media/meals/tkxquw1628771028.jpg',
+  },
+  {
+    idMeal: '53065',
+    strMeal: 'Sushi',
+    strDrinkAlternate: null,
+    strCategory: 'Seafood',
+    strArea: 'Japanese',
+    strMealThumb: 'https://www.themealdb.com/images/media/meals/g046bb1663960946.jpg',
+  },
+  {
+    idMeal: '52978',
+    strMeal: 'Kumpir',
+    strCategory: 'Side',
+    strArea: 'Turkish',
+    strMealThumb: 'https://www.themealdb.com/images/media/meals/mlchx21564916997.jpg',
+  }],
+};
+
+export const drinksDataMock = {
+  drinks: [
+    {
+      idDrink: '15997',
+      strDrink: 'GG',
+      strCategory: 'Ordinary Drink',
+      strAlcoholic: 'Optional alcohol',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+    },
+    {
+      idDrink: '17222',
+      strDrink: 'A1',
+      strCategory: 'Cocktail',
+      strAlcoholic: 'Alcoholic',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
+    },
+    {
+      idDrink: '17225',
+      strDrink: 'Ace',
+      strCategory: 'Cocktail',
+
+      strAlcoholic: 'Alcoholic',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/l3cd7f1504818306.jpg',
+    },
+    {
+      idDrink: '14229',
+      strDrink: '747',
+      strDrinkAlternate: null,
+      strCategory: 'Shot',
+      strAlcoholic: 'Alcoholic',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/xxsxqy1472668106.jpg',
+    }] };
+
+export const beefMealsDataMock = {
+  meals: [
+    {
+      strMeal: 'Beef and Mustard Pie',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg',
+      idMeal: '52874',
+    },
+    {
+      strMeal: 'Beef and Oyster pie',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wrssvt1511556563.jpg',
+      idMeal: '52878',
+    },
+    {
+      strMeal: 'Beef Asado',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/pkopc31683207947.jpg',
+      idMeal: '53071',
+    },
+    {
+      strMeal: 'Beef Banh Mi Bowls with Sriracha Mayo, Carrot & Pickled Cucumber',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/z0ageb1583189517.jpg',
+      idMeal: '52997',
+    },
+    {
+      strMeal: 'Beef Bourguignon',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/vtqxtu1511784197.jpg',
+      idMeal: '52904',
+    },
+    {
+      strMeal: 'Beef Brisket Pot Roast',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/ursuup1487348423.jpg',
+      idMeal: '52812',
+    },
+    {
+      strMeal: 'Beef Caldereta',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/41cxjh1683207682.jpg',
+      idMeal: '53070',
+    },
+    {
+      strMeal: 'Beef Dumpling Stew',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/uyqrrv1511553350.jpg',
+      idMeal: '52873',
+    },
+    {
+      strMeal: 'Beef Lo Mein',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1529444830.jpg',
+      idMeal: '52952',
+    },
+    {
+      strMeal: 'Beef Mechado',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/cgl60b1683206581.jpg',
+      idMeal: '53068',
+    },
+    {
+      strMeal: 'Beef Rendang',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/bc8v651619789840.jpg',
+      idMeal: '53053',
+    },
+    {
+      strMeal: 'Beef stroganoff',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/svprys1511176755.jpg',
+      idMeal: '52834',
+    },
+    {
+      strMeal: 'Beef Sunday Roast',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/ssrrrs1503664277.jpg',
+      idMeal: '52824',
+    },
+    {
+      strMeal: 'Beef Wellington',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/vvpprx1487325699.jpg',
+      idMeal: '52803',
+    },
+  ] };
+
+export const breakfastMealsDataMock = {
+  meals: [
+    {
+      strMeal: 'Breakfast Potatoes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1550441882.jpg',
+      idMeal: '52965',
+    },
+    {
+      strMeal: 'English Breakfast',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/utxryw1511721587.jpg',
+      idMeal: '52895',
+    },
+    {
+      strMeal: 'Fruit and Cream Cheese Breakfast Pastries',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1543774956.jpg',
+      idMeal: '52957',
+    },
+    {
+      strMeal: 'Full English Breakfast',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/sqrtwu1511721265.jpg',
+      idMeal: '52896',
+    },
+    {
+      strMeal: 'Home-made Mandazi',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/thazgm1555350962.jpg',
+      idMeal: '52967',
+    },
+    {
+      strMeal: 'Salmon Eggs Eggs Benedict',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1550440197.jpg',
+      idMeal: '52962',
+    },
+    {
+      strMeal: 'Smoked Haddock Kedgeree',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1550441275.jpg',
+      idMeal: '52964',
+    },
+  ],
+};
+
+export const cocktailsDrinksMock = {
+  drinks: [
+    {
+      strDrink: '155 Belmont',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg',
+      idDrink: '15346',
+    },
+    {
+      strDrink: '57 Chevy with a White License Plate',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/qyyvtu1468878544.jpg',
+      idDrink: '14029',
+    },
+    {
+      strDrink: '747 Drink',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/i9suxb1582474926.jpg',
+      idDrink: '178318',
+    },
+    {
+      strDrink: '9 1/2 Weeks',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/xvwusr1472669302.jpg',
+      idDrink: '16108',
+    },
+    {
+      strDrink: "A Gilligan's Island",
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/wysqut1461867176.jpg',
+      idDrink: '16943',
+    },
+    {
+      strDrink: 'A True Amaretto Sour',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/rptuxy1472669372.jpg',
+      idDrink: '17005',
+    },
+    {
+      strDrink: 'A.D.M. (After Dinner Mint)',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/ruxuvp1472669600.jpg',
+      idDrink: '14560',
+    },
+    {
+      strDrink: 'A1',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
+      idDrink: '17222',
+    },
+    {
+      strDrink: 'Abbey Martini',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/2mcozt1504817403.jpg',
+      idDrink: '17223',
+    },
+    {
+      strDrink: 'Absolut Summertime',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/trpxxs1472669662.jpg',
+      idDrink: '14107',
+    },
+    {
+      strDrink: 'Absolutely Fabulous',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/abcpwr1504817734.jpg',
+      idDrink: '17224',
+    },
+    {
+      strDrink: 'Absolutly Screwed Up',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/yvxrwv1472669728.jpg',
+      idDrink: '16134',
+    },
+    {
+      strDrink: 'Ace',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/l3cd7f1504818306.jpg',
+      idDrink: '17225',
+    },
+  ] };
